91963a2db56c3d22b6d3466f03a223b35bf4691f
Jaeyoung Lee <jaeyoung@monodiary.net>
2015-12-29 13:49:58 +0900

    Change name of parsers

--- crawler/parser/__init__.py
+++ crawler/parser/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from crawler.parser.naver import NaverComicParser, NaverWebtoonParser
-from crawler.parser.daum import DaumWebtoonParser
+from crawler.parser.naver import NaverComicParser, NaverNovelParser
+from crawler.parser.daum import DaumComicParser
 from crawler.parser.gae9 import Gae9NovelParser
 
-__all__ = ['NaverComicParser', 'NaverWebtoonParser',
-           'DaumWebtoonParser',
+__all__ = ['NaverComicParser', 'NaverNovelParser',
+           'DaumComicParser',
            'Gae9NovelParser',
            ]
